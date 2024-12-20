cad765508da1441e77885c5959aec89630d9487e
Marie Rivalin <rivalin.marie@gmail.com>
2024-12-20 12:13:46 +0100

    cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,19 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("dceabwngj"),
+        api_key: env("333851933352676"),
+        api_secret: env("l1xbTQOL5YZJ830Zf_mI439kCwI"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {
+          timeout: 1000 * 60 * 3,
+        },
+        delete: {},
+      },
+    },
+  },
+});
